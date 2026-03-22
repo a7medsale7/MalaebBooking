f5b99536b9a7e52a6aece90e0d275460436e4bac
ahmed saleh <125801796+a7medsale7@users.noreply.github.com>
2026-03-22 22:10:13 +0200

    add pagination,sprt,search Not Applyed

---
+++ MalaebBooking.Application/Abstractions/PaginatedList.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalaebBooking.Application.Abstractions;
+public class PaginatedList<T>
+{
+    public IReadOnlyList<T> Items { get; }
+    public int PageIndex { get; }
+    public int TotalPages { get; }
+    public int TotalCount { get; }
+
+    public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+    {
+        Items = items;
+        PageIndex = pageIndex;
+        TotalCount = count;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+    }
+
+    public bool HasPreviousPage => PageIndex > 1;
+    public bool HasNextPage => PageIndex < TotalPages;
+
+    public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+    {
+        if (pageSize <= 0)
+            throw new ArgumentException("Page size must be greater than zero.");
+
+        if (pageIndex <= 0)
+            pageIndex = 1;
+
+        var count = await source.CountAsync();
+
+        var items = await source
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PaginatedList<T>(items, count, pageIndex, pageSize);
+    }
+}
---
+++ MalaebBooking.Application/Contracts/ReqeustFilters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalaebBooking.Application.Contracts;
+public class ReqeustFilters
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+
+    public string SearchValue { get; init; } = string.Empty;
+    public string SortColumn { get; init; } = string.Empty;
+    public string SortDirection { get; init; } = "asc";
+
+}
